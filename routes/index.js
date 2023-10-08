5d8125548947d061df40bad550a432a32c47e190
Gaston Larriera <glarriera@Gastons-MacBook-Pro.local>
2023-10-08 18:41:34 -0300

    Delete public folder

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express')
+var pkg = require('../package.json')
+
 var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+  res.send({
+    name: pkg.name,
+    version: pkg.version,
+    enviroment: process.env.ENV,
+  })
 })
 
 module.exports = router
