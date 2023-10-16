3eab02d193a721692a223e34afc825c210b1b595
Gaston Larriera <glarriera@Gastons-MacBook-Pro.local>
2023-10-16 18:54:52 -0300

    add dotenv and env variables

--- routes/user.js
+++ routes/user.js
@@ -6,8 +6,17 @@ router.get('/', function (req, res) {
   res.send('respond with a resource')
 })
 
+router.get('/:id', async function (req, res) {
+  const users = await User.find(req.params)
+  if (!users || users.length == 0) {
+    res.status(404).send('User not found')
+  } else {
+    res.send(users)
+  }
+})
+
 router.get('/all', async function (req, res) {
-  const users = await User.find()
+  const users = await User.find({})
   res.send(users)
 })
 
