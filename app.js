bb0ce1e0255f29c19ee60ef1a46ba0fc263e1257
Gaston Larriera <glarriera@Gastons-MacBook-Pro.local>
2023-10-27 20:15:36 -0300

    Add auth route and token generation

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
 const statusRouter = require('./routes/status')
+const authRouter = require('./routes/auth')
 const userRouter = require('./routes/user')
+const authentication = require('./middlewares/authentication')
+const authorization = require('./middlewares/authorization')
 
 const app = express()
 
 app.use(logger('dev'))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(authorization)
 
 // This is to aviod error
 app.get('/favicon.ico', (req, res) => res.status(204))
 
 app.use('/', statusRouter)
-app.use('/users', userRouter)
+app.use('/auth', authRouter)
+app.use('/users', authentication, userRouter)
 
 module.exports = app
