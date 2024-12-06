110ec63c02dcbbfabd18b1c9cb2120dfc4c94479
Luismaticax <luis.maticax@gmail.com>
2024-12-06 00:10:50 -0300

    completed role

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 import cors from 'cors'
 
-import statusRouter from './routes/status.js'
-import authRouter from './routes/auth.js'
-import userRouter from './routes/user.js'
-import roleRouter from "./routes/role.js";
+import {authRoutes, statusRoutes, userRoutes, roleRoutes} from './routes/index.js'
 import authentication from './middlewares/authentication.js'
 import authorization from './middlewares/authorization.js'
 
@@ -19,9 +16,9 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(authorization)
 
-app.use('/', statusRouter)
-app.use('/auth', authRouter)
-app.use('/users', authentication, userRouter)
-app.use('/role', roleRouter)
+app.use('/', statusRoutes)
+app.use('/auth', authRoutes)
+app.use('/users', authentication, userRoutes)
+app.use('/role', roleRoutes)
 
 export default app
