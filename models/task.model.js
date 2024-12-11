b65f2230e03cf40a254bc2c1e5545e2972022a7f
Luismaticax <luis.maticax@gmail.com>
2024-12-11 03:01:47 -0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml
    #	app.js
    #	controllers/team.controller.js
    #	routes/index.js
    #	services/team.service.js

--- models/task.model.js
+++ models/task.model.js
@@ -7,18 +7,28 @@ const { ObjectId } = Schema.Types
 const taskSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String },
-  priority: { type: String, enum: ['low', 'medium', 'high', 'critical'], required: true },
-  status: { type: String, enum: ['pending', 'in progress', 'completed'], default: 'pending' },
+  priority: {
+    type: String,
+    enum: ['low', 'medium', 'high', 'critical'],
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'in progress', 'completed'],
+    default: 'pending',
+  },
   completionPercentage: { type: Number, default: 0, min: 0, max: 100 },
   weight: { type: Number, default: 0, min: 0, max: 100 },
   project: { type: ObjectId, ref: 'Project', required: true }, // Ref to project collection
   assignedTo: { type: ObjectId, ref: 'GUser' }, //Ref to GUser collection
   startDate: { type: Date },
   dueDate: { type: Date },
+  isOverdue: { type: Boolean, default: false },
   createdBy: { type: ObjectId, ref: 'GUser', required: true },
   createdOn: { type: Date, default: Date.now },
   subTasks: [{ type: ObjectId, ref: 'Task' }],
   updatedOn: { type: Date, default: Date.now },
+  lastUpdatedBy: { type: ObjectId, ref: 'GUser' },
 })
 
 taskSchema.plugin(updateTimestamps)
